3468c3b160ceee5720522cbdc4c6f86a7081d5a7
akadri01 <abdelaziz.kadri94@gmail.com>
2019-05-14 22:21:36 +0200

    changes

--- pages/components/property-listing/_filter-form.js
+++ pages/components/property-listing/_filter-form.js
@@ -2,15 +2,12 @@ import { locationTownSelectField, minPrice, maxPrice, premisesTypeSelectField } 
 
 import { filterFormSubmit } from "../../helpers/form-handles.js";
 import { searchFormSubmit } from "../../helpers/form-handles.js";
-import {
-  renderCheckbox
-} from "../../helpers/reduxForm";
-import {
-  featuresCheckboxFieldList
-} from "../shared/data";
+
 
 export default () => {
   return (
+    <div className="nav">
+
     <section className="listing__filter">
             <form className="listing__filter__form" id="searchForm" onSubmit={searchFormSubmit}>
               
@@ -18,10 +15,30 @@ export default () => {
                   <option value="sale">HABITER</option>
                   <option value="rent">INVESTIR</option>
               </select>
-              <input type="search" id="address-input" placeholder="         PARIS" />
-              <input type="number" id="Prix" min="1" step="1000" placeholder="     Prix Max" />
-              <button type="submit">RECHERCHE Classique</button>
+
               
+
+              <input type="search" id="address-input" placeholder="         PARIS" />
+            
+              <select name="premises_type">
+              <option value="house" selected>
+              MAISON
+            </option>
+            <option value="flat">APPARTEMENT</option>
+            <option value="office">LOFT</option>
+            <option value="land">BIENS DE PRESTIGE</option>
+              </select>
+
+              <input type="number" id="Prix" min="1" step="1000" placeholder="     BUDGET MAX" />
+
+              <input name="advert_type" placeholder=" SURFACE MINIMALE EN M²" type="number" min="20" max="1000"/> 
+
+
+              <button type="submit">RECHERCHE CLASSIQUE</button>
+              
+              
+
+
             </form>
       <button
         className="listing__filter--expose-filter-btn"
@@ -39,82 +56,54 @@ export default () => {
         onSubmit={filterFormSubmit}
       >
         <div className="redux-input-container">
-          <label>Type de biens:</label>
+          <label></label>
           <select name="premises_type">
             <option value="house" selected>
-              Maison
+            CRITERES PRIORITAIRES
             </option>
-            <option value="flat">Appartement</option>
-            <option value="office">Loft</option>
-            <option value="land">Biens de prestige</option>
-          </select>
-        </div>
-        <div className="redux-input-container">
-                    <label>Nombre de pièces:</label>
-                    <input name="advert_type" placeholder="minimum" type="number" min="1" max="10"/> 
-        </div>
-        <div className="redux-input-container">
-                    <label> </label>
-                    <input name="advert_type" placeholder="maximum" type="number" min="1" max="10"/> 
-        </div>
-        <div className="redux-input-container">
-                    <label>Surface en m²:</label>
-                    <input name="advert_type" placeholder="minimale" type="number" min="20" max="1000"/> 
-        </div>
-        <div className="redux-input-container">
-                    <label> </label>
-                    <input name="advert_type" placeholder="maximale" type="number" min="20" max="1000"/>            
-        </div>
-
-        <div className="redux-input-container">
-          <label>Situé au minimum au</label>
-          <select name="premises_type">
-            <option value="house" selected>
-              1er étage
-            </option>
-            <option value="flat">2</option>
-            <option value="office">3</option>
-            <option value="land">4</option>
-            <option value="building">5</option>
-            <option value="surface">6</option>
-            <option value="shop">7+</option>
+            <option value="flat">TEST 2</option>
+            <option value="office">TEST 3</option>
+            <option value="land">TEST 4</option>
+            <option value="building"> TEST 5</option>
+            <option value="surface"> TEST 6</option>
+            <option value="shop">TEST 7+</option>
           </select>
         </div>
 
         <div className="redux-input-container">
-                    <label>Rez de chaussée:</label>
-                    <input name="advert_type" type="checkbox"/>
-                    <label for="scales"></label>                 
+          <label></label>
+          <select name="premises_type">
+            <option value="house" selected>
+            CRITERES FACULTATIFS
+            </option>
+            <option value="flat">TEST 2</option>
+            <option value="office">TEST 3</option>
+            <option value="land">TEST 4</option>
+            <option value="building">TEST 5</option>
+            <option value="surface">TEST 6</option>
+            <option value="shop">TEST 7+</option>
+          </select>
         </div>
 
         <div className="redux-input-container">
-                    <label>biens exposés Nord:</label>
-                    <input name="advert_type" type="checkbox"/>
-                    <label for="scales"></label>                 
+          <label></label>
+          <select name="premises_type">
+            <option value="house" selected>
+            CRITERES REDHIBITOIRES
+            </option>
+            <option value="flat">TEST 2</option>
+            <option value="office">TEST 3</option>
+            <option value="land">TEST 4</option>
+            <option value="building">TEST 5</option>
+            <option value="surface">TEST 6</option>
+            <option value="shop">TEST 7+</option>
+          </select>
         </div>
-
-        <div className="redux-input-container">
-                    <label>Cuisine Fermée:</label>
-                    <input name="advert_type" type="checkbox"/>
-                    <label for="scales"></label>                 
-        </div>
-
-        <div className="redux-input-container">
-                    <label>Nombre de balcons minimum:</label>
-                    <input name="advert_type" placeholder="minimum" type="number" min="1" max="10"/> 
-        </div>
-        <div className="redux-input-container">
-                    <label>Nombre de parkings minimum:</label>
-                    <input name="advert_type" placeholder="minimum" type="number" min="1" max="10"/> 
-        </div>
-        <div className="redux-input-container">
-                    <label>Nombre de caves minimum:</label>
-                    <input name="advert_type" placeholder="minimum" type="number" min="1" max="10"/> 
-        </div>
-
-        
-        <button className="listing__filter-form-btn">Recherche avancée</button>
-      </form>
+    
+    <button className="listing__filter-form-btn">Recherche avancée</button>
+    </form>
     </section>
+    </div>
+    
   );
 };
