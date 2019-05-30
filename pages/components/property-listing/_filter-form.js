afb5fc5f9e09bdbbddc4e4d83f05cf19ea96e6fa
akadri01 <abdelaziz.kadri94@gmail.com>
2019-05-30 08:10:31 +0200

    removing critearia search

--- pages/components/property-listing/_filter-form.js
+++ pages/components/property-listing/_filter-form.js
@@ -42,69 +42,7 @@ export default () => {
 
 
             </form>
-      <form
-        className="listing__filter-form default-redux-form"
-        id="filterForm"
-        onSubmit={filterFormSubmit}
-      >
-        <div className="redux-input-container">
-          <label></label>
-          <select name="premises_type">
-            <option value="house" selected>
-            Critères prioritaires
-            </option>
-            
-            <option value="flat">TEST 2</option>
-            <option value="office">TEST 3</option>
-            <option value="land">TEST 4</option>
-            <option value="building"> TEST 5</option>
-            <option value="surface"> TEST 6</option>
-            <option value="shop">TEST 7</option>
-            <option value="shop">TEST 8</option>
-            <option value="shop">TEST 9</option>
-            <option value="shop">TEST 10</option>
-          </select>
-        </div>
-
-        <div className="redux-input-container">
-          <label></label>
-          <select name="premises_type">
-            <option value="house" selected>
-            Critères facultatifs
-            </option>
-            <option value="flat">TEST 2</option>
-            <option value="office">TEST 3</option>
-            <option value="land">TEST 4</option>
-            <option value="building">TEST 5</option>
-            <option value="surface">TEST 6</option>
-            <option value="shop">TEST 7</option>
-            <option value="shop">TEST 8</option>
-            <option value="shop">TEST 9</option>
-            <option value="shop">TEST 10</option>
-          </select>
-        </div>
-
-        <div className="redux-input-container">
-          <label></label>
-          <select name="premises_type">
-            <option value="house" selected>
-            Critères rédhibitoires
-            </option>
-            <option value="flat">TEST 2</option>
-            <option value="office">TEST 3</option>
-            <option value="land">TEST 4</option>
-            <option value="building">TEST 5</option>
-            <option value="surface">TEST 6</option>
-            <option value="shop">TEST 7</option>
-            <option value="shop">TEST 8</option>
-            <option value="shop">TEST 9</option>
-            <option value="shop">TEST 10</option>
-          </select>
-        </div>
-    
-        <button className="listing__filter-form-btn">Chasseur virtuel</button>
-        
-    </form>
+      
     </section>
     </div>
      );
